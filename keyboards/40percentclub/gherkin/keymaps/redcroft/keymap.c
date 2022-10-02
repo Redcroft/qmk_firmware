3e89d3c0dbccfa4ba5937932ac0a2d79e40e208e
Richard DW Redcroft <Richard@redcroft.tech>
2022-10-02 22:19:44 +0100

    Swap pgdn and pgup

--- keyboards/40percentclub/gherkin/keymaps/redcroft/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/redcroft/keymap.c
@@ -14,12 +14,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_BSPC,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,
   KC_LSFT, KC_Z,    ALT_X,   F1_SPC,  F2_C,    KC_V,    F3_B,    KC_N,    KC_M,    KC_BSPC
-  ),
+                          ),
 
   [2] = LAYOUT_ortho_3x10(
   KC_TAB,  KC_GRV,  KC_LBRC, KC_RBRC, KC_EQL,  KC_SCLN, KC_HOME, KC_UP,   KC_END,  KC_BSPC,
   KC_LCTL, KC_BSLS, KC_SLSH, KC_ESC,  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_MINS,
-  KC_LSFT, KC_LGUI, KC_LALT, _______, KC_DEL,  _______, KC_PGDN, _______, KC_PGUP, KC_ENT
+  KC_LSFT, KC_LGUI, KC_LALT, _______, KC_DEL,  _______, KC_PGUP, _______, KC_PGDN, KC_ENT
                           ),
 
   [3] = LAYOUT_ortho_3x10(
@@ -27,7 +27,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
   KC_LSFT, KC_LGUI, KC_LALT, _______, KC_DEL,  KC_LCA,  KC_COMM, KC_DOT,  KC_F11,  KC_F12
                           ),
-
 
   [4] = LAYOUT_ortho_3x10(
   KC_ESC,  DF(0),   DF(1),   _______, _______, _______, _______, KC_UP,   _______, KC_BSPC,
