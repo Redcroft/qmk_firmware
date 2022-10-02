d6e42eef3ca5224a0aac59a39591f84f6923a98d
Richard DW Redcroft <Richard@redcroft.tech>
2022-10-02 23:04:53 +0100

    Swapped PGUP/PGDN as it felt weird the other way

--- keyboards/40percentclub/gherkin/keymaps/redcroft/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/redcroft/keymap.c
@@ -17,15 +17,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           ),
 
   [2] = LAYOUT_ortho_3x10(
-  KC_TAB,  KC_GRV,  KC_LBRC, KC_RBRC, KC_EQL,  KC_SCLN, KC_HOME, KC_UP,   KC_END,  KC_BSPC,
+  KC_TAB,  KC_GRV,  KC_LBRC, KC_RBRC, KC_EQL,  KC_SCLN, KC_END,  KC_UP,   KC_HOME, KC_BSPC,
   KC_LCTL, KC_BSLS, KC_SLSH, KC_ESC,  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_MINS,
-  KC_LSFT, KC_LGUI, KC_LALT, _______, KC_DEL,  _______, KC_PGUP, _______, KC_PGDN, KC_ENT
+  KC_LSFT, KC_LGUI, KC_LALT, _______, KC_DEL,  _______, KC_PGDN , _______, KC_PGUP, KC_ENT
                           ),
 
   [3] = LAYOUT_ortho_3x10(
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  KC_LSFT, KC_LGUI, KC_LALT, _______, KC_DEL,  KC_LCA,  KC_COMM, KC_DOT,  KC_F11,  KC_F12
+  KC_LSFT, KC_LGUI, KC_LALT, _______, _______, KC_LCA,  KC_COMM, KC_DOT,  KC_F11,  KC_F12
                           ),
 
   [4] = LAYOUT_ortho_3x10(
