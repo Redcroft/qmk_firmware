4113ff3f76112646c599f62e522ea0fa6b9de2d4
Richard DW Redcroft <Richard@redcroft.tech>
2023-09-13 10:22:19 +0100

    Update to latest config

--- keyboards/crkbd/keymaps/redcroft/keymap.c
+++ keyboards/crkbd/keymaps/redcroft/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
 			   ESC_CTL,  KC_A,  KC_R,   KC_S,   KC_T,   KC_G,              KC_M,   KC_N,   KC_E,   KC_I,   KC_O, KC_MINUS, \
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
-			   KC_LSFT,  KC_Z,  KC_X,   KC_C,   KC_D,   KC_V,              KC_K,   KC_H, KC_COMM,KC_DOT,KC_SLSH,KC_SFTENT, \
+			   KC_LSFT,  KC_Z,  KC_X,   KC_C,   KC_D,   KC_V,              KC_K,   KC_H, KC_COMM,KC_DOT,KC_SLSH,SC_SENT, \
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
 			   KC_LALT, KC_SPC, SUPL,   MO(3), KC_BSPC, KC_LGUI \
 			   //`-----------------------'  `----------------------'
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
 			   KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,              KC_H,   KC_J,   KC_K,   KC_L, KC_SCLN, KC_MINUS,\
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
-			   KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,              KC_N,   KC_M, KC_COMM,KC_DOT,KC_SLSH,KC_SFTENT,\
+			   KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,              KC_N,   KC_M, KC_COMM,KC_DOT,KC_SLSH,SC_SENT,\
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
 			   KC_LALT, KC_SPC, SUPL,   MO(3), KC_BSPC, KC_LGUI \
 			   //`-----------------------'  `----------------------'
--- keyboards/crkbd/keymaps/redcroft/rules.mk
+++ keyboards/crkbd/keymaps/redcroft/rules.mk
@@ -1,7 +1,7 @@
 MOUSEKEY_ENABLE = no     # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes    # Enable WS2812 RGB underlight.
 VIA_ENABLE      = yes
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+#OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 CONVERT_TO      = kb2040
