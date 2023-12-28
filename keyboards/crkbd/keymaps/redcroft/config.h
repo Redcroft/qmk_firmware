edbba7fec72515a364d888dca77a2e5ab57e82dd
Richard DW Redcroft <Richard@redcroft.tech>
2023-12-28 19:14:20 +0000

    Remove some RGB Modes

--- keyboards/crkbd/keymaps/redcroft/config.h
+++ keyboards/crkbd/keymaps/redcroft/config.h
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 //#define USE_MATRIX_I2C
+#define ENABLE_COMPILE_KEYCODE
 
 /* Select hand configuration */
 
@@ -33,14 +34,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    #define RGBLIGHT_EFFECT_SNAKE
-    #define RGBLIGHT_EFFECT_KNIGHT
+    #undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #undef RGBLIGHT_EFFECT_SNAKE
+    #undef RGBLIGHT_EFFECT_KNIGHT
     #define RGBLIGHT_EFFECT_CHRISTMAS
     #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    #define RGBLIGHT_EFFECT_RGB_TEST
-    #define RGBLIGHT_EFFECT_ALTERNATING
+    #undef RGBLIGHT_EFFECT_RGB_TEST
+    #undef RGBLIGHT_EFFECT_ALTERNATING
     #define RGBLIGHT_EFFECT_TWINKLE
     #define RGBLIGHT_LIMIT_VAL 120
     #define RGBLIGHT_HUE_STEP 5
--- keyboards/crkbd/keymaps/redcroft/keymap.c
+++ keyboards/crkbd/keymaps/redcroft/keymap.c
@@ -24,26 +24,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KCT KC_TRNS
 #define SUPL MO(2) //LT(_LOWER, KC_LGUI)
 
+const uint8_t RGB_LED_BREATHING_INTERVALS[] PROGMEM = {50, 30};
+const uint8_t RGBLED_TWINKLE_INTERVALS[] PROGMEM = {50, 25};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
 			   //,-----------------------------------------------.          ,-----------------------------------------------.
-			   KC_TAB,   KC_Q,  KC_W,   KC_F,   KC_P,   KC_B,              KC_J,   KC_L,   KC_U,   KC_Y,  KC_SCLN,KC_QUOT,\
+			   KC_TAB,   KC_Q,  KC_W,   KC_F,   KC_P,   KC_B,                KC_J,   KC_L,   KC_U,   KC_Y,  KC_SCLN,KC_QUOT,\
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
-			   ESC_CTL,  KC_A,  KC_R,   KC_S,   KC_T,   KC_G,              KC_M,   KC_N,   KC_E,   KC_I,   KC_O, KC_MINUS, \
+			   ESC_CTL,  KC_A,  KC_R,   KC_S,   KC_T,   KC_G,                KC_M,   KC_N,   KC_E,   KC_I,   KC_O, KC_MINUS, \
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
-			   KC_LSFT,  KC_Z,  KC_X,   KC_C,   KC_D,   KC_V,              KC_K,   KC_H, KC_COMM,KC_DOT,KC_SLSH,SC_SENT, \
+			   KC_LSFT,  KC_Z,  KC_X,   KC_C,   KC_D,   KC_V,                KC_K,   KC_H, KC_COMM,KC_DOT,KC_SLSH,SC_SENT, \
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
 			   KC_LALT, KC_SPC, SUPL,   MO(3), KC_BSPC, KC_LGUI \
 			   //`-----------------------'  `----------------------'
 			   ),
   [1] = LAYOUT_split_3x6_3(
 			   //,-----------------------------------------------.          ,-----------------------------------------------.
-			   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,              KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,  KC_QUOT,\
+			   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,  KC_QUOT,\
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
-			   KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,              KC_H,   KC_J,   KC_K,   KC_L, KC_SCLN, KC_MINUS,\
+			   KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                KC_H,   KC_J,   KC_K,   KC_L, KC_SCLN, KC_MINUS,\
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
-			   KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,              KC_N,   KC_M, KC_COMM,KC_DOT,KC_SLSH,SC_SENT,\
+			   KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                KC_N,   KC_M, KC_COMM,KC_DOT,KC_SLSH,SC_SENT,\
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
 			   KC_LALT, KC_SPC, SUPL,   MO(3), KC_BSPC, KC_LGUI \
 			   //`-----------------------'  `----------------------'
@@ -63,11 +65,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [3] = LAYOUT_split_3x6_3(
 			   //,-----------------------------------------------.          ,-----------------------------------------------.
-			   KC_GRV,  KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,            KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_EQL,\
+			   KC_GRV,  KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,             KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_EQL,\
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
-			   KCT,     RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,DF(0),              KCT,   KC_LEFT,KC_DOWN, KC_UP, KC_RIGHT,KC_RCTL, \
+			   KCT,     RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,DF(0),               KCT,   KC_LEFT,KC_DOWN, KC_UP, KC_RIGHT,KC_RCTL, \
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
-			   KCT,     RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,DF(1),              KC_DEL,KC_HOME,KC_PGDN,KC_PGUP,KC_END,  KCT,\
+			   KCT,     RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,DF(1),               KC_DEL,KC_HOME,KC_PGDN,KC_PGUP,KC_END,  QK_RBT,\
 			   //|-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------|
 			   KCT, KCT, KCT,           KCT, KCT, KCT
 			   //`-----------------------'  `----------------------'
