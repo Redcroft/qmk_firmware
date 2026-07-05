2caa2fcb784f0328062ae611a988bf3f4d799e73
Richard DW Redcroft <Richard@redcroft.tech>
2026-07-05 16:21:02 +0100

    Remove vol keys

--- keyboards/crkbd/keymaps/redcroft_41/keymap.c
+++ keyboards/crkbd/keymaps/redcroft_41/keymap.c
@@ -57,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                   `-----------------'                 `-----------------'
    */
   [_COLEMAK] = LAYOUT_split_3x6_3_ex2(
-				      KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,  KC_VOLU,     KC_BRIU, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT,
-				      KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,  KC_VOLD,     KC_BRID, KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_MNS,
-				      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                        KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, SC_SENT,
+				      KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,  KC_UP,       KC_BRIU, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT,
+				      KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,  KC_DOWN,     KC_BRID, KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_MNS,
+				      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                        KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
 								KC_LALT, KC_SPC,  MO(3),                        MO(4),   KC_BSPC, KC_LGUI
 				      ),
 
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT_split_3x6_3_ex2(
 				    KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
 				    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,     _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
-				    _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_ESC,  KC_LGUI,                       _______, _______, KC_LBRC, KC_RBRC, KC_HASH, KC_BSLS,
+				    _______, KC_MUTE, _______, _______, KC_ESC,  KC_LGUI,                       _______, _______, KC_LBRC, KC_RBRC, KC_HASH, KC_BSLS,
 				    _______, _______, _______,          _______, _______, _______
 				    ),
 
